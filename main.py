7283814fcb2d3f5385f1b6031147cbc620087141
Daniel Flynn <flynn.da@northeastern.edu>
2024-08-11 12:42:46 -0400

    Slight change to main

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from pokergame import *
 
 
-agents = [Agent(f"Agent {i+1}", [preflop_PM, flop_PM, turn_PM, river_PM]) for i in range(10)]
-max_agent_number = 10
+agents_in_game = 10
+agents = [Agent(f"Agent {i+1}", [preflop_PM, flop_PM, turn_PM, river_PM]) for i in range(agents_in_game)]
 
 game = PokerGame(agents)
 game.play_game()
