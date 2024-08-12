a83cadbdcddbbbcd85db5852579082ebae3f0409
Daniel Flynn <flynn.da@northeastern.edu>
2024-08-12 11:57:39 -0400

    There is now a game loop

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from pokergame import *
 
 
+def get_balance(agent: Agent):
+    return agent.balance
+
+
+def create_mutated_agent(agent: Agent):
+    # Implement the mutations here
+    return agent
+
+
 agents_amount = 10
 agents = [Agent(f"Agent {i+1}", [preflop_PM, flop_PM, turn_PM, river_PM]) for i in range(agents_amount)]
+total_agents_used = agents_amount
 
-game = PokerGame(agents)
-game.play_game()
+for i in range(1000):
+    game = PokerGame(agents)
+    game.play_game()
+    agents = game.agents
+    while len(agents) < 10:
+        agents.append(create_mutated_agent(max(agents, key=get_balance)))
